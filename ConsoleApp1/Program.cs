2e1ca222cefbdeab32fcaec7c2cfa516c872c2ea
Max <prostakovmaksym@gmail.com>
2022-09-17 03:32:12 +0300

    "first commit"

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿using ConsoleApp1;
+
+var path = Environment.GetCommandLineArgs()[1];
+var newPath = Environment.GetCommandLineArgs()[2];
+var shiftMs = Convert.ToInt32(Environment.GetCommandLineArgs()[3]);
+
+SubtitleModifier.ExecuteSubtitleShift(path, newPath, shiftMs);
