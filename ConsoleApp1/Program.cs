1dc038863dbe244162f56670c2c41d21f2f6a875
Max <prostakovmaksym@gmail.com>
2022-09-18 23:14:48 +0300

    remove garbage

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,4 +4,6 @@ var path = Environment.GetCommandLineArgs()[1];
 var newPath = Environment.GetCommandLineArgs()[2];
 var shiftMs = Convert.ToInt32(Environment.GetCommandLineArgs()[3]);
 
-SubtitleModifier.ExecuteSubtitleShift(path, newPath, shiftMs);
+var input = File.ReadAllText(path);
+var output = SubtitleModifier.ExecuteSubtitleShift(input, shiftMs);
+File.WriteAllText(newPath, output);
--- ConsoleApp1/SubtitleModifier.cs
+++ ConsoleApp1/SubtitleModifier.cs
@@ -7,22 +7,24 @@ public static class SubtitleModifier
 {
     private const string TimeFormat = @"hh\:mm\:ss\,fff";
     private static readonly Regex TimeRegex = new(@"(\d\d):(\d\d):(\d\d),(\d\d\d)");
-
-    // TODO:
-    // create git repo
-    // push this project
-    // create gitignore file
-    public static void ExecuteSubtitleShift(string path, string newPath, int shiftMs)
-    {
-        var input = File.ReadAllText(path);
-        input = TimeRegex.Replace(input, m => AddTime(m, shiftMs));
-        File.WriteAllText(newPath, input);
-    }
-
+    
     private static string AddTime(Match m, int shiftMs)
     {
         var t = TimeSpan.ParseExact(m.Value, TimeFormat, CultureInfo.InvariantCulture);
         t += new TimeSpan(0, 0, 0, 0, shiftMs);
         return t.ToString(TimeFormat);
     }
+    
+    public static string ExecuteSubtitleShift(string source, int shiftMs)
+    {
+        var result = TimeRegex.Replace(source, m => AddTime(m, shiftMs));
+        return result;
+    }
+    
+    /*public static void ExecuteSubtitleShift(string path, string newPath, int shiftMs)
+    {
+        var input = File.ReadAllText(path);
+        input = TimeRegex.Replace(input, m => AddTime(m, shiftMs));
+        File.WriteAllText(newPath, input);
+    }*/
 }
