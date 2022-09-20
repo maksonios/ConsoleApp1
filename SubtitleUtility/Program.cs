c7bc0d7fbdcae3193fa90e4fe92b0a8801549431
Max <prostakovmaksym@gmail.com>
2022-09-21 02:05:18 +0300

    Add numbering parameter

--- SubtitleUtility/Program.cs
+++ SubtitleUtility/Program.cs
@@ -7,6 +7,6 @@ var newPath = Environment.GetCommandLineArgs()[2];
 var shiftMs = Convert.ToInt32(Environment.GetCommandLineArgs()[3]);
 var subsNumering = Convert.ToBoolean(Environment.GetCommandLineArgs()[4]);
 
-var input = File.ReadAllText(path);
+var input = File.ReadAllLines(path);
 var output = SubtitleModifier.ExecuteSubtitleShift(input, shiftMs, subsNumering);
 File.WriteAllText(newPath, output);
--- SubtitleUtility/SubtitleModifier.cs
+++ SubtitleUtility/SubtitleModifier.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace SubtitleUtility;
@@ -9,10 +10,34 @@ public static class SubtitleModifier
     private const string TimeFormat = @"hh\:mm\:ss\,fff";
     private static readonly Regex TimeRegex = new(@"(\d\d):(\d\d):(\d\d),(\d\d\d)");
     
-    public static string ExecuteSubtitleShift(string source, int shiftMs, bool isSubtitleNumeringEnabled = true)
+    public static string ExecuteSubtitleShift(string[] source, int shiftMs, bool isSubtitleNumberingEnabled = true)
     {
-        var result = TimeRegex.Replace(source, m => AddTime(m, shiftMs));
-        return result;
+        var newStr = new StringBuilder();
+        if (!isSubtitleNumberingEnabled)
+        {
+            string matchSymbol = "-->";
+            for (int i = 0; i < source.Length-1; i++)
+            {
+                if (source[i+1].Contains(matchSymbol))
+                {
+                    var timeline=TimeRegex.Replace(source[i+1], m => AddTime(m, shiftMs));
+                    newStr.AppendLine(timeline);
+                    continue;
+                }
+                if (source[i].Contains(matchSymbol))
+                {
+                    continue;
+                }
+                newStr.AppendLine(source[i]);
+            }
+        }
+        else
+        {
+            var result = TimeRegex.Replace(string.Join("\n",source), m => AddTime(m, shiftMs));
+            return result;
+        }
+
+        return newStr.ToString();
     }
     
     private static string AddTime(Match m, int shiftMs)
