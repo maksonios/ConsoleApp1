eb1ba306511b2b96ea682cd1e2cfc974bfd03f2c
Max <prostakovmaksym@gmail.com>
2022-09-22 05:09:58 +0300

    Add tests for isSubtitleNumberingEnabled param

--- SubtitleUtility.Tests/SubtitleShiftTests.cs
+++ SubtitleUtility.Tests/SubtitleShiftTests.cs
@@ -21,7 +21,9 @@ public class SubtitleShiftTests
                                            "4" + Environment.NewLine + 
                                            "00:05:05,847 --> 00:05:07,190" + Environment.NewLine + 
                                            "Challenge round, Billy." + Environment.NewLine;
-    private const int ShiftMs = 500; 
+    private const int ShiftMs = 500;
+    private const bool IsNumerationEnabled = true;
+    private const string DefaultTimeIntervalDelimiter = "-->";
 
     [Test]
     public void ExecuteSubtitleShift_ShouldShiftSubtitlesForward_WhenShiftParamIsPositive()
@@ -77,5 +79,49 @@ public class SubtitleShiftTests
     [Test]
     public void ExecuteSubtitleShift_ShouldEnableSubtitleNumeration_WhenBoolParamIsTrue()
     {
+        var expectedValue = "1" + Environment.NewLine + 
+                            "00:02:24,896 --> 00:02:26,773" + Environment.NewLine + 
+                            "Master Kaecilius." + Environment.NewLine + 
+                            "" + Environment.NewLine + 
+                            "2" + Environment.NewLine + 
+                            "00:02:27,899 --> 00:02:31,745" + Environment.NewLine + 
+                            "That ritual will bring you only sorrow." + Environment.NewLine + 
+                            "" + Environment.NewLine + 
+                            "3" + Environment.NewLine + 
+                            "00:03:07,730 --> 00:03:08,947" + Environment.NewLine + 
+                            "Hypocrite!" + Environment.NewLine + 
+                            "" + Environment.NewLine + 
+                            "4" + Environment.NewLine + 
+                            "00:05:05,347 --> 00:05:06,690" + Environment.NewLine + 
+                            "Challenge round, Billy." + Environment.NewLine;
+
+        var actualValue = SubtitleModifier.ExecuteSubtitleShift(Input, -ShiftMs);
+
+        actualValue.Should().BeEquivalentTo(expectedValue);
+    }
+    
+    [Test]
+    public void ExecuteSubtitleShift_ShouldEnableSubtitleNumeration_WhenBoolParamIsFalse()
+    {
+        var expectedValue = "00:02:24,896 --> 00:02:26,773" + Environment.NewLine + 
+                            "Master Kaecilius." + Environment.NewLine + 
+                            "" + Environment.NewLine +
+                            "00:02:27,899 --> 00:02:31,745" + Environment.NewLine + 
+                            "That ritual will bring you only sorrow." + Environment.NewLine + 
+                            "" + Environment.NewLine +
+                            "00:03:07,730 --> 00:03:08,947" + Environment.NewLine + 
+                            "Hypocrite!" + Environment.NewLine + 
+                            "" + Environment.NewLine +
+                            "00:05:05,347 --> 00:05:06,690" + Environment.NewLine + 
+                            "Challenge round, Billy." + Environment.NewLine; 
+
+
+        var actualValue = SubtitleModifier.ExecuteSubtitleShift(Input,
+                                                                -ShiftMs,
+                                                                DefaultTimeIntervalDelimiter,
+                                                                DefaultTimeIntervalDelimiter,
+                                                                !IsNumerationEnabled);
+
+        actualValue.Should().BeEquivalentTo(expectedValue);
     }
 }
